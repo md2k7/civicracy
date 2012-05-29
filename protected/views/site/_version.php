5b841c21830aeb2b6f89f2ab5b0e417f25d5bbb1
David Madl <freeware@abanbytes.eu>
2012-05-29 13:25:42 +0200

    version fixed

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,3 +1,2 @@
-Civi Version 0.1.2
-/*.NOTE:.1 vom 29.05.2012 13:24:44
+Civi Version 0.1.4 vom 29.05.2012 13:27:47
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
