0acb5cea9be2a160dfc93fc453b1c20b8b5abbdf
David Madl <freeware@abanbytes.eu>
2012-05-29 16:56:39 +0200

    sql: use database

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.10 vom 29.05.2012 16:54:56
+Civi Version 0.1.11 vom 29.05.2012 16:56:39
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
