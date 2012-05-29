43ed621bf0a4b8141be25c85795bef63b13e14fd
David Madl <freeware@abanbytes.eu>
2012-05-29 13:21:33 +0200

    index: version file

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,1 +1,2 @@
-<p>Version 0.2 vom 29.05.2012 13:18:33</p>
+<p>Version 0.1.1 vom 29.05.2012 13:21:33</p>
+<?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
