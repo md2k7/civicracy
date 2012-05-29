f82dafbc0499aa621d8d269b693ad94b1cf2f43e
David Madl <freeware@abanbytes.eu>
2012-05-29 16:53:55 +0200

    removed data for sqlite

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.8 vom 29.05.2012 16:26:29
+Civi Version 0.1.9 vom 29.05.2012 16:53:55
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
