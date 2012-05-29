b892737066e78601895b884b88ebf6f9763c93be
David Madl <freeware@abanbytes.eu>
2012-05-29 17:56:25 +0200

    deploy.sh: fetch application.log

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.21 vom 29.05.2012 17:37:05
+Civi Version 0.1.22 vom 29.05.2012 17:56:25
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
