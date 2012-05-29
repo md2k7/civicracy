8ef0086c2f4bd48a685ec6ab452649d67f0bba8e
David Madl <freeware@abanbytes.eu>
2012-05-29 13:24:44 +0200

    version into footer

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,3 @@
-<p>Version 0.1.1 vom 29.05.2012 13:21:33</p>
+Civi Version 0.1.2
+/*.NOTE:.1 vom 29.05.2012 13:24:44
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
