0ca6a557840547183677618b0c1e828b8921a19b
David Madl <freeware@abanbytes.eu>
2012-05-29 17:11:40 +0200

    deploy.sh fixed

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.14 vom 29.05.2012 16:57:55
+Civi Version 0.1.15 vom 29.05.2012 17:11:40
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
