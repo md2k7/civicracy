a2302a0930f627f2d92c397706b7b7b7fb38c127
David Madl <freeware@abanbytes.eu>
2012-06-14 23:42:00 +0200

    deploy.sh: added git commit hook as a comment

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.26 vom 14.06.2012 23:38:19
-<?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number is kept, c is incremented. */ ?>
+Civi Version 0.1.27 vom 14.06.2012 23:42:00
+<?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
