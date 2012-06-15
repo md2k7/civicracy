b5fecdf816b08fb2cbfc582fcc3385062d1d8041
David Madl <freeware@abanbytes.eu>
2012-06-16 01:16:03 +0200

    sorted language files alphabetically

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.34 vom 16.06.2012 01:13:11
+Civi Version 0.1.35 vom 16.06.2012 01:16:03
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
