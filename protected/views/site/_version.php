fcf46cd2a4a7c2968e2e5b83ff5525c92ec78096
David Madl <freeware@abanbytes.eu>
2012-06-16 16:25:26 +0200

    deploy.sh: chown www-data of the logfile

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.36 vom 16.06.2012 16:25:09
+Civi Version 0.1.37 vom 16.06.2012 16:25:26
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
