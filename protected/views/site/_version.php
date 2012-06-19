44a15c55650993376373def911f7a0527a1203bc
David Madl <freeware@abanbytes.eu>
2012-06-19 14:26:57 +0200

    deploy.sh: local deploy feature added

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.64 vom 19.06.2012 13:48:07
+Civi Version 0.1.65 vom 19.06.2012 14:26:57
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
