b62f66e0da29c67c136cd060a8b0f42edf8f85f1
David Madl <freeware@abanbytes.eu>
2012-06-19 14:27:07 +0200

    copied production.php to civi.php, config too

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.65 vom 19.06.2012 14:26:57
+Civi Version 0.1.67 vom 19.06.2012 14:28:30
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
