78bdda38bd0a8f79397d7bff53be215209c92f88
David Madl <freeware@abanbytes.eu>
2012-06-19 13:23:25 +0200

    deploy.sh: host adapted

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.60 vom 19.06.2012 13:20:08
+Civi Version 0.1.61 vom 19.06.2012 13:23:25
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
