9afd7626c41aaecf55e8d069129f875f6c28466e
David Madl <freeware@abanbytes.eu>
2012-06-19 13:41:58 +0200

    fixed DB schema

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.62 vom 19.06.2012 13:38:48
+Civi Version 0.1.63 vom 19.06.2012 13:41:58
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
