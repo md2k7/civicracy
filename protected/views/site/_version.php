b5d27f1fc65809f6fd11363a486c78dd1966c2ec
David Madl <freeware@abanbytes.eu>
2012-06-19 13:48:07 +0200

    assets removed from git

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.63 vom 19.06.2012 13:41:58
+Civi Version 0.1.64 vom 19.06.2012 13:48:07
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
