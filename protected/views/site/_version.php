5197b6901090817c6499d68536c12282a0c35d07
David Madl <freeware@abanbytes.eu>
2012-06-19 14:43:44 +0200

    DB schema: create 4 test users and 1 test category

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.70 vom 19.06.2012 14:36:32
+Civi Version 0.1.71 vom 19.06.2012 14:43:44
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
