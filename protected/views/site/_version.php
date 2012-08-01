6ac979be0f48c240a066de9368e2c691bad598ab
David Madl <freeware@abanbytes.eu>
2012-08-01 18:19:44 +0200

    deploy.sh comment

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.80 vom 19.06.2012 16:29:12
+Civi Version 0.1.81 vom 01.08.2012 18:19:44
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
