24c3ddb1f27d3b24153822ffda50ce9ed1d890d3
David Madl <freeware@abanbytes.eu>
2012-10-02 20:39:07 +0200

    rename to Civicracy

--- protected/views/site/_version.php
+++ protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.82 vom 01.08.2012 18:25:30
+Civi Version 0.1.83 vom 02.10.2012 20:39:07
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
