79112955badec074e63a50d913b7cea8daa37c8d
David Madl <freeware@abanbytes.eu>
2012-10-06 16:12:32 +0200

    added git commit hook to repo

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.85 vom 06.10.2012 16:10:56
+Civi Version 0.1.86 vom 06.10.2012 16:12:32
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
