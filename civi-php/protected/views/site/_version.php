991be09179551cb44ab7c56135454871094065dd
David Madl <freeware@abanbytes.eu>
2012-10-23 18:11:14 +0200

    design2: added navbar

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civi Version 0.1.86 vom 06.10.2012 16:12:32
+Civi Version 0.1.88 vom 23.10.2012 18:11:14
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
