4af85ae2a9dcad5a7f692bf735cb46298a89ebf1
David Madl <freeware@abanbytes.eu>
2012-10-28 21:32:17 +0100

    git post-commit: redirect output unless there is a problem

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.18 vom 28.10.2012 21:24:21
+Civicracy Version 0.2.19 vom 28.10.2012 21:32:17
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
