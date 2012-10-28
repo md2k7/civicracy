5713a8b50e3ee9d5275129c507bb7d42430f8598
David Madl <freeware@abanbytes.eu>
2012-10-28 14:30:47 +0100

    controllers: use main layout instead of column2 which breaks stuff atm

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.5 vom 28.10.2012 14:09:13
+Civicracy Version 0.2.6 vom 28.10.2012 14:30:47
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
