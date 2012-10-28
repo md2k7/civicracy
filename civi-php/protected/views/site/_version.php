7c1a88f9cf4cedad32ca7036182a7e69adb6d1f1
David Madl <freeware@abanbytes.eu>
2012-10-28 18:41:33 +0100

    git hook: pre-commit: don't bump version number if not on branch master

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.15 vom 28.10.2012 18:39:02
+Civicracy Version 0.2.16 vom 28.10.2012 18:41:33
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
