6db51c453252952743d92de287ccacdf14c38357
David Madl <freeware@abanbytes.eu>
2012-10-28 14:07:35 +0100

    main.css: adapt vote combo for in-form usage - grr

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.3 vom 28.10.2012 14:02:47
+Civicracy Version 0.2.4 vom 28.10.2012 14:07:35
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
