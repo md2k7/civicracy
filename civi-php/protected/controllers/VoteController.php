220d58fa75c3c3489885979efb6f1ff02b69435c
David Madl <freeware@abanbytes.eu>
2012-10-28 19:23:06 +0100

    VoteController: log errors to 'votecontroller'

--- civi-php/protected/controllers/VoteController.php
+++ civi-php/protected/controllers/VoteController.php
@@ -121,7 +121,7 @@ class VoteController extends Controller
 						$this->redirect(array('view','id'=>$id));
 						$transaction->commit();
 					} else {
-						Yii::error($historyModel->getErrors(), 'vc');
+						Yii::error($historyModel->getErrors(), 'votecontroller');
 						$transaction->rollBack();
 					}
 				} else {
