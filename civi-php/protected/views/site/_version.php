3ba147b74769fb2c564f5f100aed4c911e2a56e6
David Madl <freeware@abanbytes.eu>
2012-10-28 18:42:56 +0100

    todo updated

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.16 vom 28.10.2012 18:41:33
+Civicracy Version 0.2.17 vom 28.10.2012 18:42:56
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
