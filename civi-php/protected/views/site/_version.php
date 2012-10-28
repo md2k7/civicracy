a600e135f3e532c62e09cd341d835664e892ff9a
David Madl <freeware@abanbytes.eu>
2012-10-28 15:48:49 +0100

    js: add jquery and bootstrap

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.10 vom 28.10.2012 15:48:36
+Civicracy Version 0.2.11 vom 28.10.2012 15:48:49
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
