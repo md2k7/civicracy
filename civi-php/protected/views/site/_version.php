4a3f3557a9e409b5716b02a4ffc1d2600c4e10be
David Madl <freeware@abanbytes.eu>
2012-10-28 21:35:16 +0100

    git post-commit: more silence

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.20 vom 28.10.2012 21:33:19
+Civicracy Version 0.2.21 vom 28.10.2012 21:35:16
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
