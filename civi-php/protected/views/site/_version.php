9af2a35acd67fa747220cc7e149a6225c39e5e87
David Madl <freeware@abanbytes.eu>
2012-10-31 18:53:00 +0100

    design2 deleted, it is already integrated into civi-php templates

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.33 vom 29.10.2012 22:45:03
+Civicracy Version 0.2.34 vom 31.10.2012 18:53:00
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
