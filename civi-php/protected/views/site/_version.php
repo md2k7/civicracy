e5855019983314c0147e00c6d5011cbccfc6fabe
David Madl <freeware@abanbytes.eu>
2012-10-31 23:10:37 +0100

    config: add example.php

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.34 vom 31.10.2012 18:53:00
+Civicracy Version 0.2.35 vom 31.10.2012 23:10:37
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
