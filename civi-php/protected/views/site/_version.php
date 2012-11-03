93ef86d179fc0e561875446bcbcf728ca26d187c
David Madl <freeware@abanbytes.eu>
2012-11-03 15:04:11 +0100

    gitignore: ignore yii folder

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.35 vom 31.10.2012 23:10:37
+Civicracy Version 0.2.36 vom 03.11.2012 15:04:11
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
