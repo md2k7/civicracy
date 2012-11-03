49b085604b9cf27d3fe86650ca2c1108a1472a86
David Madl <freeware@abanbytes.eu>
2012-11-03 16:45:18 +0100

    DB model: removed autoincrement numbers from 01-initial.sql

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.37 vom 03.11.2012 16:43:46
+Civicracy Version 0.2.38 vom 03.11.2012 16:45:18
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
