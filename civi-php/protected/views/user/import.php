027a90df8bb1a45aa0020a00983436f519a583ff
jakaura <devnull@extremst.org>
2012-11-05 22:29:24 +0100

    Added ability to upload CSV files - will be saved in folder civi-php/csvfiles Userdata is being extracted into an array
    Todo: Create Users with Userdata from CSV File

--- civi-php/protected/views/user/import.php
+++ civi-php/protected/views/user/import.php
@@ -1,13 +1,13 @@
-	<div class="main-content">
+ <div class="main-content">
 <?php
 $this->breadcrumbs=array(
 	Yii::t('app', 'menu.users') => array('admin'),
-	$model->username,
 );
 
 ?>
 	</div>
 		
 	<h1><?php echo Yii::t('app', 'menu.users.import'); ?></h1>
-
-<?php echo $this->renderPartial('importForm', array('model'=>$model)); ?>
+<div class="form">
+<?php echo $form; ?>
+</div>
