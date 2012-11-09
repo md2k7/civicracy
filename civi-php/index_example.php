795e20e229cfc6fb96e1092acdcd485642491af8
David Madl <freeware@abanbytes.eu>
2012-11-09 16:00:51 +0100

    index_example.php: use absolute path via dirname(__FILE__)

--- civi-php/index_example.php
+++ civi-php/index_example.php
@@ -1,8 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yii='/framework/yii.php';
-$config='/protected/config/example.php';
+$yii=dirname(__FILE__).'/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/example.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
