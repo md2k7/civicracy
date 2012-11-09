42067ba0201a4d8609829e3c4dd05ec47f9383de
David Madl <freeware@abanbytes.eu>
2012-11-09 15:13:09 +0100

    i18n: messages/de/app.php merge fix - re-added my changes from before

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.51 vom 09.11.2012 14:52:09
+Civicracy Version 0.2.52 vom 09.11.2012 15:13:09
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
