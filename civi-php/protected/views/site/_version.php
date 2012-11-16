f41361c9048bde45da6ffe401f6e8e47df051486
David Madl <freeware@abanbytes.eu>
2012-11-16 10:53:14 +0100

    bump version

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.67 vom 13.11.2012 14:03:17
+Civicracy Version 0.2.69 vom 16.11.2012 10:53:14
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
