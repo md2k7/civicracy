136b14aa9124a15482e474da962314c0eba789b4
David Madl <freeware@abanbytes.eu>
2012-11-16 14:24:34 +0100

    views: pull out hero unit into separate view in views/vote confirm, update, view, updateoptions

--- civi-php/protected/views/vote/confirm.php
+++ civi-php/protected/views/vote/confirm.php
@@ -10,25 +10,7 @@ $this->menu=array(
 );
 
 ?>
-		<div class="hero-unit">
-			<div class="hero-left">
-				<div class="hero-title">
-					<h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
-					<p><?php echo Yii::t('app', 'site.motto'); ?></p>
-				</div>
-				<div class="hero-logo">
-					<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/civicracy.png" alt="Kreise" />
-				</div>
-			</div>
-			<div class="hero-right hero-title-right">
-				<h2><?php echo CHtml::encode($category->institution); ?></h2>
-				<h3><?php echo CHtml::encode($category->name); ?></h3>
-				<div class="hero-description">
-					<p><?php echo CHtml::encode($category->description); ?></p>
-				</div>
-			</div>
-			<div class="container"></div>
-		</div>
+		<?php echo $this->renderPartial('heroUnit', array('category' => $category)); ?>
 		<div class="main-content">
 			<div class="responsibility">
 				<h4><?php echo Yii::t('app', 'vote.ownWeight'); ?></h4>
--- civi-php/protected/views/vote/updateoptions.php
+++ civi-php/protected/views/vote/updateoptions.php
@@ -6,25 +6,7 @@ $this->breadcrumbs=array(
 );
 
 ?>
-		<div class="hero-unit">
-			<div class="hero-left">
-				<div class="hero-title">
-					<h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
-					<p><?php echo Yii::t('app', 'site.motto'); ?></p>
-				</div>
-				<div class="hero-logo">
-					<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/civicracy.png" alt="Kreise" />
-				</div>
-			</div>
-			<div class="hero-right hero-title-right">
-				<h2><?php echo CHtml::encode($category->institution); ?></h2>
-				<h3><?php echo CHtml::encode($category->name); ?></h3>
-				<div class="hero-description">
-					<p><?php echo CHtml::encode($category->description); ?></p>
-				</div>
-			</div>
-			<div class="container"></div>
-		</div>
+		<?php echo $this->renderPartial('heroUnit', array('category' => $category)); ?>
 		<div class="main-content">
 			<h4><?php echo Yii::t('app', 'menu.categoryVoteUpdate', array('{category}' => $category->name)); ?></h4>
 			<ul>
