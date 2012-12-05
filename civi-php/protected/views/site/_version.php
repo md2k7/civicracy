a75d12125b4db597005060d8341e206d2a943556
David Madl <freeware@abanbytes.eu>
2012-12-05 09:33:49 +0100

    fixed faq typo

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.81 vom 05.12.2012 09:20:43
+Civicracy Version 0.2.82 vom 05.12.2012 09:33:49
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
