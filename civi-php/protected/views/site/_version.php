c946920ea514abd3a627917fbdd9b1e578a9c8d4
David Madl <freeware@abanbytes.eu>
2013-04-20 22:30:19 +0200

    add warning regarding activation mail for gmail users
    Conflicts:
    	civi-php/protected/views/user/de/createSuccess.php

--- civi-php/protected/views/site/_version.php
+++ civi-php/protected/views/site/_version.php
@@ -1,2 +1,2 @@
-Civicracy Version 0.2.97 vom 20.04.2013 21:55:38
+Civicracy Version 0.2.98 vom 20.04.2013 22:31:13
 <?php /* NOTE: this file is auto-generated. The digits a and b of the a.b.c version number are kept, c is incremented. */ ?>
--- civi-php/protected/views/user/de/createSuccess.php
+++ civi-php/protected/views/user/de/createSuccess.php
@@ -1,4 +1,5 @@
 		<div class="main-content">
 			<h1>Benutzer erfolgreich erstellt</h1>
 			<p>Bitte prüfe Deine E-Mails, um den gerade erstellten Benutzer zu aktivieren.</p>
+			<p><strong>An Nutzer von Google Mail:</strong> Bitte im Spam-Ordner nachsehen. Bei Problemen: Mail an <a href="mailto:support@civicracy.at">support@civicracy.at</a>.</p>
 		</div>
